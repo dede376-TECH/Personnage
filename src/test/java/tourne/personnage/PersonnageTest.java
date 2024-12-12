a831da7a964929f7dd4a7a70a93a9682eb1c0c9a
dede376-TECH <khamlicheabdelghani5@gmail.com>
2024-12-12 19:39:35 +0100

    Ajout du test pour tourner(1) retournant EST

--- src/test/java/tourne/personnage/PersonnageTest.java
+++ src/test/java/tourne/personnage/PersonnageTest.java
@@ -3,9 +3,6 @@ package tourne.personnage;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-
-import static org.junit.jupiter.api.Assertions.*;
-
 public class PersonnageTest {
     @Test
     public void tourner_1_quart_retourne_EST() {
@@ -13,11 +10,4 @@ public class PersonnageTest {
         String orientation = personnage.tourner(1);
         assertThat(orientation).isEqualTo("EST");
     }
-    @Test
-    public void tourner_2_quarts_retourne_SUD() {
-        Personnage personnage = new Personnage();
-        String orientation = personnage.tourner(2);
-        assertThat(orientation).isEqualTo("SUD");
-    }
-
 }
